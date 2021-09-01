045f2d5468c684b58272492446a588e565c59a11
386083520 <386083520@qq.com>
2021-09-02 07:35:23 +0800

    在线，离线逻辑处理

--- bin/vue-init.js
+++ bin/vue-init.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
+const download = require('download-git-repo')
 const program = require('commander')
 const chalk = require('chalk')
 const path = require('path')
 const home = require('user-home')
 const tildify = require('tildify')
+const ora = require('ora')
 const exists = require('fs').existsSync
 const inquirer = require('inquirer')
+const rm = require('rimraf').sync
 const logger = require('../lib/logger')
+const generate = require('../lib/generate')
+const checkVersion = require('../lib/check-version')
+const warnings = require('../lib/warnings')
+const localPath = require('../lib/local-path')
+const isLocalPath = localPath.isLocalPath
+const getTemplatePath = localPath.getTemplatePath
 
 program
     .usage('<template-name> [project-name]')
@@ -32,14 +41,18 @@ help()
 
 let template = program.args[0]
 console.log('gsdtemplate', template)
+const hasSlash = template.indexOf('/') > -1
+console.log('gsdhasSlash', hasSlash)
 const rawName = program.args[1]
 console.log('gsdrawName', rawName)
 const inPlace = !rawName || rawName === '.'
 console.log('gsdinPlace', inPlace)
 const name = inPlace ? path.relative('../', process.cwd()) : rawName
-console.log('gsdname', name, process.cwd())
+console.log('gsdname', name)
 const to = path.resolve(rawName || '.')
 console.log('gsdto', to)
+const clone = program.clone || false
+console.log('gsdclone', clone)
 const tmp = path.join(home, '.vue-templates', template.replace(/[\/:]/g, '-'))
 console.log('gsdtmp', tmp)
 console.log('gsdoffline', program.offline)
@@ -66,5 +79,45 @@ if (inPlace || exists(to)) {
 }
 
 function run () {
-    console.log('gsdrun')
+    if (isLocalPath(template)) {
+        const templatePath = getTemplatePath(template)
+        console.log('gsdtemplatePath', templatePath)
+        if (exists(templatePath)) {
+            generate(name, templatePath, to, err => {
+
+            })
+        } else {
+            logger.fatal('Local template "%s" not found.', template)
+        }
+    } else {
+        checkVersion(() => {
+            if (!hasSlash) {
+                const officialTemplate = 'vuejs-templates/' + template
+                console.log('gsdofficialTemplate', officialTemplate)
+                if (template.indexOf('#') !== -1) {
+                    downloadAndGenerate(officialTemplate)
+                } else {
+                    if (template.indexOf('-2.0') !== -1) {
+                        warnings.v2SuffixTemplatesDeprecated(template, inPlace ? '' : name)
+                        return
+                    }
+                    downloadAndGenerate(officialTemplate)
+                }
+            } else {
+                downloadAndGenerate(template)
+            }
+        })
+    }
+}
+
+function downloadAndGenerate (template) {
+    const spinner = ora('downloading template')
+    spinner.start()
+    if (exists(tmp)) rm(tmp)
+    download(template, tmp, { clone }, err => {
+        spinner.stop()
+        if (err) logger.fatal('Failed to download repo ' + template + ': ' + err.message.trim())
+        generate(name, tmp, to, err => {
+        })
+    })
 }
